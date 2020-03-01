a352ae5ff81adbbdb7c7043f59eae615a23e778b
Eli White <github@eli-white.com>
2020-02-29 19:06:55 -0800

    Updates

---
+++ precache-manifest.f006290b382d39e8edebc8250d1994a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "988d571b739b422ed8e3561074c33b2f",
+    "url": "/covid19-changes/index.html"
+  },
+  {
+    "revision": "eb3c713db4b6b98d4d08",
+    "url": "/covid19-changes/static/css/main.91f9855a.chunk.css"
+  },
+  {
+    "revision": "24d3ea5574e9cb3d2fee",
+    "url": "/covid19-changes/static/js/2.11cd4552.chunk.js"
+  },
+  {
+    "revision": "1d801b267c376d8d847057f45bc75646",
+    "url": "/covid19-changes/static/js/2.11cd4552.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb3c713db4b6b98d4d08",
+    "url": "/covid19-changes/static/js/main.34ac949d.chunk.js"
+  },
+  {
+    "revision": "ff7be7395d39218016d4",
+    "url": "/covid19-changes/static/js/runtime-main.cfa3a40e.js"
+  }
+]);

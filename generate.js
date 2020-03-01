ebffc2a1ea4e7c1834e9917a99b15c599896aa92
Eli White <github@eli-white.com>
2020-02-29 18:37:29 -0800

    Initial commit

---
+++ generate.js
@@ -0,0 +1,73 @@
+const { parse } = require('node-html-parser');
+const fetch = require('node-fetch');
+const fs = require('fs');
+const path = require('path');
+
+const currentFolder = __dirname;
+const CDC_UPDATES_PATH = path.join(__dirname, 'cdcupdates');
+
+// prettier-ignore
+const dates = {
+  '20-01-21': 'https://web.archive.org/web/20200121230721/https://www.cdc.gov/coronavirus/2019-nCoV/summary.html',
+  '20-01-22': 'https://web.archive.org/web/20200122074254/https://www.cdc.gov/coronavirus/2019-nCoV/summary.html',
+  '20-01-23': 'https://web.archive.org/web/20200123210102/https://www.cdc.gov/coronavirus/2019-nCoV/summary.html',
+  '20-01-24': 'https://web.archive.org/web/20200124213411/https://www.cdc.gov/coronavirus/2019-nCoV/summary.html',
+  '20-01-25': 'https://web.archive.org/web/20200125235002/https://www.cdc.gov/coronavirus/2019-nCoV/summary.html',
+  '20-01-26': 'https://web.archive.org/web/20200126210549/https://www.cdc.gov/coronavirus/2019-nCoV/summary.html',
+  '20-01-27': 'https://web.archive.org/web/20200127124834/https://www.cdc.gov/coronavirus/2019-nCoV/summary.html',
+  '20-01-28': 'https://web.archive.org/web/20200128220500/https://www.cdc.gov/coronavirus/2019-nCoV/summary.html',
+  '20-01-29': 'https://web.archive.org/web/20200129210515/https://www.cdc.gov/coronavirus/2019-nCoV/summary.html',
+  '20-01-30': 'https://web.archive.org/web/20200130235421/https://www.cdc.gov/coronavirus/2019-nCoV/summary.html',
+  '20-01-31': 'https://web.archive.org/web/20200131191845/https://www.cdc.gov/coronavirus/2019-nCoV/summary.html',
+  '20-02-01': 'https://web.archive.org/web/20200201222850/https://www.cdc.gov/coronavirus/2019-nCoV/summary.html',
+  '20-02-02': 'https://web.archive.org/web/20200202221311/https://www.cdc.gov/coronavirus/2019-nCoV/summary.html',
+  '20-02-03': 'https://web.archive.org/web/20200203145915/https://www.cdc.gov/coronavirus/2019-nCoV/summary.html',
+  '20-02-04': 'https://web.archive.org/web/20200204095343/https://www.cdc.gov/coronavirus/2019-nCoV/summary.html',
+  '20-02-05': 'https://web.archive.org/web/20200205233924/https://www.cdc.gov/coronavirus/2019-nCoV/summary.html',
+  '20-02-06': 'https://web.archive.org/web/20200206223631/https://www.cdc.gov/coronavirus/2019-nCoV/summary.html',
+  '20-02-07': 'https://web.archive.org/web/20200207213651/https://www.cdc.gov/coronavirus/2019-nCoV/summary.html',
+  '20-02-08': 'https://web.archive.org/web/20200208152846/https://www.cdc.gov/coronavirus/2019-nCoV/summary.html',
+  '20-02-09': 'https://web.archive.org/web/20200209224736/https://www.cdc.gov/coronavirus/2019-nCoV/summary.html',
+  '20-02-10': 'https://web.archive.org/web/20200210164201/https://www.cdc.gov/coronavirus/2019-nCoV/summary.html',
+  '20-02-11': 'https://web.archive.org/web/20200211173545/https://www.cdc.gov/coronavirus/2019-nCoV/summary.html',
+  '20-02-12': 'https://web.archive.org/web/20200212153047/https://www.cdc.gov/coronavirus/2019-nCoV/summary.html',
+  '20-02-13': 'https://web.archive.org/web/20200213164308/https://www.cdc.gov/coronavirus/2019-nCoV/summary.html',
+  '20-02-14': 'https://web.archive.org/web/20200214204733/https://www.cdc.gov/coronavirus/2019-nCoV/summary.html',
+  '20-02-15': 'https://web.archive.org/web/20200215213934/https://www.cdc.gov/coronavirus/2019-nCoV/summary.html',
+  '20-02-16': 'https://web.archive.org/web/20200216201049/https://www.cdc.gov/coronavirus/2019-nCoV/summary.html',
+  '20-02-17': 'https://web.archive.org/web/20200218032523/https://www.cdc.gov/coronavirus/2019-nCoV/summary.html',
+  '20-02-18': 'https://web.archive.org/web/20200219053517/https://www.cdc.gov/coronavirus/2019-nCoV/summary.html',
+  '20-02-19': 'https://web.archive.org/web/20200220182904/https://www.cdc.gov/coronavirus/2019-nCoV/summary.html',
+  '20-02-20': 'https://web.archive.org/web/20200221182709/https://www.cdc.gov/coronavirus/2019-nCoV/summary.html',
+  '20-02-21': 'https://web.archive.org/web/20200222215422/https://www.cdc.gov/coronavirus/2019-nCoV/summary.html',
+  '20-02-22': 'https://web.archive.org/web/20200223170732/https://www.cdc.gov/coronavirus/2019-nCoV/summary.html',
+  '20-02-23': 'https://web.archive.org/web/20200224213221/https://www.cdc.gov/coronavirus/2019-nCoV/summary.html',
+  '20-02-24': 'https://web.archive.org/web/20200225003215/https://www.cdc.gov/coronavirus/2019-nCoV/summary.html',
+}
+
+async function run() {
+  for (date in dates) {
+    const originalsPath = path.join(currentFolder, 'originals', `${date}.txt`);
+    if (fs.existsSync(originalsPath)) {
+      console.log('Already have', date, '... Skipping');
+    }
+
+    const url = dates[date];
+    const result = await fetch(url);
+    const html = await result.text();
+    const root = parse(html);
+    const content = root.querySelector('.content');
+    const cleanedContent = content.text
+      .split('\n')
+      .map(line => line.trim())
+      .filter(Boolean)
+      .join('\n');
+
+    fs.writeFileSync(
+      path.join(CDC_UPDATES_PATH, `${date}.txt`),
+      cleanedContent,
+    );
+  }
+}
+
+run();
--- src/App.js
+++ src/App.js
@@ -1,24 +1,107 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import ReactDiffViewer, { DiffMethod } from 'react-diff-viewer';
+import Dropdown from 'react-dropdown';
 import './App.css';
+import raw from 'raw.macro';
+
+const dates = {
+  '20-01-21': raw('../cdcupdates/20-01-21.txt'),
+  '20-01-22': raw('../cdcupdates/20-01-22.txt'),
+  '20-01-23': raw('../cdcupdates/20-01-23.txt'),
+  '20-01-24': raw('../cdcupdates/20-01-24.txt'),
+  '20-01-25': raw('../cdcupdates/20-01-25.txt'),
+  '20-01-26': raw('../cdcupdates/20-01-26.txt'),
+  '20-01-27': raw('../cdcupdates/20-01-27.txt'),
+  '20-01-28': raw('../cdcupdates/20-01-28.txt'),
+  '20-01-29': raw('../cdcupdates/20-01-29.txt'),
+  '20-01-30': raw('../cdcupdates/20-01-30.txt'),
+  '20-01-31': raw('../cdcupdates/20-01-31.txt'),
+  '20-02-01': raw('../cdcupdates/20-02-01.txt'),
+  '20-02-02': raw('../cdcupdates/20-02-02.txt'),
+  '20-02-03': raw('../cdcupdates/20-02-03.txt'),
+  '20-02-04': raw('../cdcupdates/20-02-04.txt'),
+  '20-02-05': raw('../cdcupdates/20-02-05.txt'),
+  '20-02-06': raw('../cdcupdates/20-02-06.txt'),
+  '20-02-07': raw('../cdcupdates/20-02-07.txt'),
+  '20-02-08': raw('../cdcupdates/20-02-08.txt'),
+  '20-02-09': raw('../cdcupdates/20-02-09.txt'),
+  '20-02-10': raw('../cdcupdates/20-02-10.txt'),
+  '20-02-11': raw('../cdcupdates/20-02-11.txt'),
+  '20-02-12': raw('../cdcupdates/20-02-12.txt'),
+  '20-02-13': raw('../cdcupdates/20-02-13.txt'),
+  '20-02-14': raw('../cdcupdates/20-02-14.txt'),
+  '20-02-15': raw('../cdcupdates/20-02-15.txt'),
+  '20-02-16': raw('../cdcupdates/20-02-16.txt'),
+  '20-02-17': raw('../cdcupdates/20-02-17.txt'),
+  '20-02-18': raw('../cdcupdates/20-02-18.txt'),
+  '20-02-19': raw('../cdcupdates/20-02-19.txt'),
+  '20-02-20': raw('../cdcupdates/20-02-20.txt'),
+  '20-02-21': raw('../cdcupdates/20-02-21.txt'),
+  '20-02-22': raw('../cdcupdates/20-02-22.txt'),
+  '20-02-23': raw('../cdcupdates/20-02-23.txt'),
+  '20-02-24': raw('../cdcupdates/20-02-24.txt'),
+};
+
+function convertShorthandToDate(shorthand) {
+  const [year, month, day] = shorthand.split('-').map(val => parseInt(val, 10));
+  return new Date('20' + year, month - 1, day).toDateString();
+}
 
 function App() {
+  const dateOptions = Object.keys(dates).reverse();
+  const [nextValue, setNextValue] = useState(dateOptions[0]);
+  const [prevValue, setPrevValue] = useState(dateOptions[1]);
+
+  const formattedOptions = dateOptions.map(dateOption => ({
+    value: dateOption,
+    label: convertShorthandToDate(dateOption),
+  }));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <h1>
+        Comparison of{' '}
+        <a href="https://www.cdc.gov/coronavirus/2019-nCoV/summary.html">
+          CDC's COVID-19 updates
         </a>
-      </header>
+      </h1>
+      <div className="date-picker-row">
+        <Dropdown
+          options={formattedOptions}
+          onChange={newValue => {
+            setPrevValue(newValue.value);
+          }}
+          value={prevValue}
+          placeholder="Select a date to compare from"
+        />
+        <Dropdown
+          options={formattedOptions}
+          onChange={newValue => {
+            setNextValue(newValue.value);
+          }}
+          value={nextValue}
+          placeholder="Select a date to compare to"
+        />
+      </div>
+      {dates[nextValue] === dates[prevValue] ? (
+        <NoUpdates prevDate={prevValue} nextDate={nextValue} />
+      ) : (
+        <ReactDiffViewer
+          oldValue={dates[prevValue]}
+          newValue={dates[nextValue]}
+          splitView={true}
+          compareMethod={DiffMethod.WORDS}
+        />
+      )}
+    </div>
+  );
+}
+
+function NoUpdates({ prevDate, nextDate }) {
+  return (
+    <div className="page-center">
+      No changes found on CDC's website found between{' '}
+      {convertShorthandToDate(prevDate)} and {convertShorthandToDate(nextDate)}
     </div>
   );
 }

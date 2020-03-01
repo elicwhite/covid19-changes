662d50de9ad4e63736832a1c4c022c189f43528a
Eli White <github@eli-white.com>
2020-02-29 19:02:48 -0800

    On load find the nearest previous day that results in a diff

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDiffViewer, { DiffMethod } from 'react-diff-viewer';
 import Dropdown from 'react-dropdown';
 import './App.css';
@@ -44,18 +44,38 @@ const dates = {
 
 function convertShorthandToDate(shorthand) {
   const [year, month, day] = shorthand.split('-').map(val => parseInt(val, 10));
-  return new Date('20' + year, month - 1, day).toDateString();
+  return new Date('20' + year, month - 1, day).toLocaleString('en-US', {
+    weekday: 'long',
+    month: 'short',
+    day: 'numeric',
+  });
 }
 
 function App() {
   const dateOptions = Object.keys(dates).reverse();
-  const [nextValue, setNextValue] = useState(dateOptions[0]);
-  const [prevValue, setPrevValue] = useState(dateOptions[1]);
+  const [nextIndex, setNextIndex] = useState(0);
+  const [prevIndex, setPrevIndex] = useState(1);
 
-  const formattedOptions = dateOptions.map(dateOption => ({
-    value: dateOption,
+  const formattedOptions = dateOptions.map((dateOption, index) => ({
+    value: index,
     label: convertShorthandToDate(dateOption),
   }));
+
+  const prevDate = dateOptions[prevIndex];
+  const nextDate = dateOptions[nextIndex];
+
+  // On mount, find the most recent previous date that results in a diff
+  useEffect(() => {
+    let prevIndexPointer = prevIndex;
+
+    while (
+      dates[dateOptions[nextIndex]] == dates[dateOptions[prevIndexPointer]]
+    ) {
+      prevIndexPointer++;
+    }
+
+    setPrevIndex(prevIndexPointer);
+  }, []);
 
   return (
     <div className="App">
@@ -69,26 +89,26 @@ function App() {
         <Dropdown
           options={formattedOptions}
           onChange={newValue => {
-            setPrevValue(newValue.value);
+            setPrevIndex(newValue.value);
           }}
-          value={prevValue}
+          value={convertShorthandToDate(prevDate)}
           placeholder="Select a date to compare from"
         />
         <Dropdown
           options={formattedOptions}
           onChange={newValue => {
-            setNextValue(newValue.value);
+            setNextIndex(newValue.value);
           }}
-          value={nextValue}
+          value={convertShorthandToDate(nextDate)}
           placeholder="Select a date to compare to"
         />
       </div>
-      {dates[nextValue] === dates[prevValue] ? (
-        <NoUpdates prevDate={prevValue} nextDate={nextValue} />
+      {dates[nextDate] === dates[prevDate] ? (
+        <NoUpdates prevDate={prevDate} nextDate={nextDate} />
       ) : (
         <ReactDiffViewer
-          oldValue={dates[prevValue]}
-          newValue={dates[nextValue]}
+          oldValue={dates[prevDate]}
+          newValue={dates[nextDate]}
           splitView={true}
           compareMethod={DiffMethod.WORDS}
         />

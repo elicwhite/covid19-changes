b07f8f260832ad301dfef7a4818e731cc2f12eb3
Vishnu Ravi <vishnu.ravi@gmail.com>
2020-03-02 10:38:51 -0500

    Compare multiple CDC pages

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import ReactGA from 'react-ga';
 import Dropdown from 'react-dropdown';
 import queryString from 'query-string';
 import './App.css';
-import CDCUpdates from './cdcUpdatesMapping';
+import CDCUpdatesSummary from './cdcUpdatesMappingSummary';
+import CDCUpdatesScreening from './cdcUpdatesMappingScreening';
+import CDCUpdatesManagement from './cdcUpdatesMappingManagement';
+import pages from './pages';
 
 function convertShorthandToDate(shorthand) {
   const [year, month, day] = shorthand.split('-').map(val => parseInt(val, 10));
@@ -15,8 +18,26 @@ function convertShorthandToDate(shorthand) {
   });
 }
 
+function getPageMapping(page) {
+  switch (page) {
+    case 'summary':
+      return CDCUpdatesSummary;
+    case 'management':
+      return CDCUpdatesManagement;
+    case 'screening':
+      return CDCUpdatesScreening;
+    default:
+      return CDCUpdatesSummary;
+  }
+}
+
 function App() {
   const query = queryString.parse(window.location.search);
+
+  var initialPage = query.page || 'summary';
+  const [page, setPage] = useState(initialPage);
+  var CDCUpdates = getPageMapping(page);
+
   const dateOptions = Object.keys(CDCUpdates).reverse();
   const initialFromIndex =
     query.from != null && dateOptions.indexOf(query.from) >= 0
@@ -26,7 +47,7 @@ function App() {
   const initialToIndex =
     query.to != null && dateOptions.indexOf(query.to) >= 0
       ? dateOptions.indexOf(query.to)
-      : 1;
+      : 0;
 
   const [nextIndex, setNextIndex] = useState(initialToIndex);
   const [prevIndex, setPrevIndex] = useState(initialFromIndex);
@@ -34,6 +55,11 @@ function App() {
   const formattedOptions = dateOptions.map((dateOption, index) => ({
     value: index,
     label: convertShorthandToDate(dateOption),
+  }));
+
+  const pageOptions = Object.keys(pages).map(page => ({
+    value: page,
+    label: pages[page].dropDownLabel,
   }));
 
   const prevDate = dateOptions[prevIndex];
@@ -49,35 +75,46 @@ function App() {
       prevIndexPointer++;
     }
     setPrevIndex(prevIndexPointer);
-  }, []);
+  }, [page]);
 
   useEffect(() => {
     window.history.replaceState(
       {
         prevDate,
         nextDate,
+        page,
       },
       '',
       '?' +
         queryString.stringify({
           from: prevDate,
           to: nextDate,
+          page: page,
         }),
     );
 
     if (!window.location.hostname.includes('localhost')) {
       ReactGA.pageview(window.location.pathname + window.location.search);
     }
-  }, [prevDate, nextDate]);
+  }, [prevDate, nextDate, page]);
 
   return (
     <div className="App">
       <h1>
-        Comparison of{' '}
-        <a href="https://www.cdc.gov/coronavirus/2019-nCoV/summary.html">
-        CDC's Clinical Guidance for COVID-19
-        </a>
+        Comparison of <a href={pages[page].url}>{pages[page].title}</a>
       </h1>
+      <div className="page-picker-row">
+        <Dropdown
+          options={pageOptions}
+          onChange={newValue => {
+            setPrevIndex(1);
+            setNextIndex(0);
+            setPage(newValue.value);
+          }}
+          value={page}
+          placeholder="Select a page"
+        />
+      </div>
       <div className="date-picker-row">
         <Dropdown
           options={formattedOptions}
---
+++ src/pages.js
@@ -0,0 +1,19 @@
+// CDC pages that we will display diffs for
+
+export default {
+    'summary' : {
+        title: 'CDC\'s COVID-19 Updates',
+        dropDownLabel: 'Situation Summary',
+        url: 'https://www.cdc.gov/coronavirus/2019-nCoV/summary.html' 
+        },
+    'management': { 
+        title: 'CDC\'s COVID-19 Clinical Management Guidelines', 
+        dropDownLabel: 'Clinical Management Guidelines',
+        url: 'https://www.cdc.gov/coronavirus/2019-ncov/hcp/clinical-guidance-management-patients.html' 
+        },
+    'screening': { 
+        title: 'CDC\'s COVID-19 Screening Criteria', 
+        dropDownLabel: 'Screening Criteria',
+        url: 'https://www.cdc.gov/coronavirus/2019-nCoV/hcp/clinical-criteria.html' 
+        }
+  }

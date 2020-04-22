9cf64dc8b09c8b9102dd95035f6df7b78e0c9d51
TheSavior <TheSavior@users.noreply.github.com>
2020-04-22 19:12:32 +0000

    deploy: c44699828b77ab662d0818f4359d73eb447e5e66

--- precache-manifest.395cd21b8c7986237efecd34cdd840a9.js
+++ precache-manifest.395cd21b8c7986237efecd34cdd840a9.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9224da71a51011b3c984598499bebba6",
+    "revision": "28f5837fb9ac5abddf2c95386820a63c",
     "url": "/covid19-changes/index.html"
   },
   {
-    "revision": "2abaa8aec976cf3b754b",
+    "revision": "3b5bfb6297a2eeef2104",
     "url": "/covid19-changes/static/css/main.df3b5196.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/covid19-changes/static/js/2.6fb1bfd4.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2abaa8aec976cf3b754b",
-    "url": "/covid19-changes/static/js/main.ccab8602.chunk.js"
+    "revision": "3b5bfb6297a2eeef2104",
+    "url": "/covid19-changes/static/js/main.f4d99d5a.chunk.js"
   },
   {
     "revision": "ff7be7395d39218016d4",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid19-changes/precache-manifest.f7dceff9e809ab2cd418ee3154a7ddce.js"
+  "/covid19-changes/precache-manifest.395cd21b8c7986237efecd34cdd840a9.js"
 );
 
 self.addEventListener('message', (event) => {

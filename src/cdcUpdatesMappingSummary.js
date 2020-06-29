57bd3464a9443dd717abeaae7a30c7f33bc1855c
Eli White <github@eli-white.com>
2020-06-29 04:13:09 +0000

    Add new CDC file

--- src/cdcUpdatesMappingSummary.js
+++ src/cdcUpdatesMappingSummary.js
@@ -164,4 +164,5 @@ export default {
   '20-06-26': raw('../cdcupdates/summary/20-06-26.txt'),
   '20-06-27': raw('../cdcupdates/summary/20-06-27.txt'),
   '20-06-28': raw('../cdcupdates/summary/20-06-28.txt'),
+  '20-06-29': raw('../cdcupdates/summary/20-06-29.txt'),
 };

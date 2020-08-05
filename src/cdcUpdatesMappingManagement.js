5e82cf854ca5b76326965d6e16fa477313a26ea5
Eli White <github@eli-white.com>
2020-08-05 04:17:44 +0000

    Add new CDC file

--- src/cdcUpdatesMappingManagement.js
+++ src/cdcUpdatesMappingManagement.js
@@ -164,4 +164,5 @@ export default {
   '20-08-02': raw('../cdcupdates/management/20-08-02.txt'),
   '20-08-03': raw('../cdcupdates/management/20-08-03.txt'),
   '20-08-04': raw('../cdcupdates/management/20-08-04.txt'),
+  '20-08-05': raw('../cdcupdates/management/20-08-05.txt'),
 };

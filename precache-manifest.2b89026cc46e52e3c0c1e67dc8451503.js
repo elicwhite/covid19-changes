f24231bc0349bf345d798d41e90475f1630d9347
TheSavior <TheSavior@users.noreply.github.com>
2020-08-29 04:15:55 +0000

    deploy: 0816d41c8a407ccc81ae7dfdc310c9a660948619

---
+++ precache-manifest.2b89026cc46e52e3c0c1e67dc8451503.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "016c557ee4fb40746450cec77d02b314",
+    "url": "/covid19-changes/index.html"
+  },
+  {
+    "revision": "5e616f98464985bac15b",
+    "url": "/covid19-changes/static/css/main.df3b5196.chunk.css"
+  },
+  {
+    "revision": "7fbd54dc85c275ab857d",
+    "url": "/covid19-changes/static/js/2.6fb1bfd4.chunk.js"
+  },
+  {
+    "revision": "1d801b267c376d8d847057f45bc75646",
+    "url": "/covid19-changes/static/js/2.6fb1bfd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e616f98464985bac15b",
+    "url": "/covid19-changes/static/js/main.122ac21e.chunk.js"
+  },
+  {
+    "revision": "ff7be7395d39218016d4",
+    "url": "/covid19-changes/static/js/runtime-main.cfa3a40e.js"
+  }
+]);

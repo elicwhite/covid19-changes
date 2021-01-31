07d9a8d8e2616d35213865c7d877077f57db80d9
TheSavior <TheSavior@users.noreply.github.com>
2021-01-31 05:33:31 +0000

    deploy: 0e5c1a2d62c9823eea96c86e70af2bbbaff77e00

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid19-changes/precache-manifest.9265a885d9d046db6adaaa02a60d77f9.js"
+  "/covid19-changes/precache-manifest.060d3ee21bc79fc496c8bd739e39b59a.js"
 );
 
 self.addEventListener('message', (event) => {

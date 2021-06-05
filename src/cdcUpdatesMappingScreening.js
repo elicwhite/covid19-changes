f13b91451302431db6b172347c86b23b3324b2a9
Eli White <github@eli-white.com>
2021-06-05 05:37:56 +0000

    Add new CDC file

--- src/cdcUpdatesMappingScreening.js
+++ src/cdcUpdatesMappingScreening.js
@@ -460,4 +460,5 @@ export default {
   '21-06-02': raw('../cdcupdates/screening/21-06-02.txt'),
   '21-06-03': raw('../cdcupdates/screening/21-06-03.txt'),
   '21-06-04': raw('../cdcupdates/screening/21-06-04.txt'),
+  '21-06-05': raw('../cdcupdates/screening/21-06-05.txt'),
 };
